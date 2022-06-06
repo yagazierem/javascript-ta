2c6c358cc1b087599e33351aebce043c98d229a3
yagazierem <adaogbonna44@gmail.com>
2022-06-06 23:51:28 +0100

    she code africa

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
 // complete the function
 function prime (num) {
   // code goes here
+
+  // take input from user
+const digit = prompt("Enter a positive Integer:");
+const number = Number(digit);
+let isPrime = true;
+
+// check if number is equal to 1
+
+if (number === 1) {
+  console.log("oops! 1 is not a Prime number");
+}
+
+// check if number is greater than 1
+else if (number > 1) {
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+      isPrime = false;
+
+    }
+  }
+  if (isPrime) {
+    console.log(`${number} is a prime number`);
+  } else {
+    console.log(`${number} is a not prime number`);
+  }
+}
+
+// check if number is less than 1
+else {
+  console.log("The number is not a prime number.");
+}
 }
 
 function solution (arg) {
